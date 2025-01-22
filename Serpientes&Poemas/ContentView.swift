2713257ff721aaceb1551d3304f937f3242d9c0e
Githubense <angelopf@outlook.com>
2025-01-22 16:55:00 +0100

    Initial Commit

---
+++ Serpientes&Poemas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Serpientes&Poemas
+//
+//  Created by Angel Adrian Pimienta Flores on 22/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Serpientes&Poemas/Serpientes_PoemasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Serpientes_PoemasApp.swift
+//  Serpientes&Poemas
+//
+//  Created by Angel Adrian Pimienta Flores on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Serpientes_PoemasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
